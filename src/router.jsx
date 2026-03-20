5f7432fc161600a32c65c13d8edac01306528b85
RL08 <dev.rl08@outlook.com>
2026-03-20 21:43:21 +0100

    feat: Add router routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,22 @@
-import App from '@/App.jsx';
 import { createBrowserRouter } from 'react-router-dom'
+import App from '@/App.jsx';
+import HomePage from '@/pages/HomePage.jsx';
+import CategoryPage from '@/pages/CategoryPage.jsx';
+import MealPage from '@/pages/MealPage.jsx';
+import FavoritePage from '@/pages/FavoritePage.jsx';
+import SearchResultPage from '@/pages/SearchResultPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "category/:name", element: <CategoryPage /> },
+      { path: "meal/:id", element: <MealPage /> },
+      { path: "favorite", element: <FavoritePage /> },
+      { path: "search", element: <SearchResultPage /> },
+    ],
   },
 ]);
 
