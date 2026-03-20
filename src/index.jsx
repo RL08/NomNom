9e9a31734100dd5b00e7297895dea23d4364dcf7
RL08 <dev.rl08@outlook.com>
2026-03-20 21:44:34 +0100

    feat: Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "@/styles/index.css";
 import router from "@/router.jsx";
+import "@/styles/index.css";
+import "@/services/fontawesome.js"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
