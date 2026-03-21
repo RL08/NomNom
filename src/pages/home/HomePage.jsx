1b77ac2bfb38e2b731377be70736d0c865430159
RL08 <dev.rl08@outlook.com>
2026-03-21 21:26:04 +0100

    feat: Add CategoryCard to HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "@/services/mealService.js";
+import CategoryCard from "@/features/category/CategoryCard.jsx";
+import "@/pages/home/HomePage.css";
+
 function HomePage() {
+  const [categories, setCategories] = useState([]);
+  const [categoryLoading, setCategoryLoading] = useState(true);
+  const [categoryError, setCategoryError] = useState(null);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const categories = await getCategories();
+        setCategories(categories);
+      } catch {
+        setCategoryError("Failed to load categories...");
+      } finally {
+        setCategoryLoading(false);
+      }
+    };
+
+    loadCategories();
+  }, []);
+
   return (
-    <>
-    
-    </>
+    <section className="categories-section">
+      <h2>Categories</h2>
+      {categoryError && <div className="error-message">{categoryError}</div>}
+      {categoryLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="categories-grid">
+          {categories.map((category) => (
+            <CategoryCard
+              category={category}
+              key={category.idCategory}
+            ></CategoryCard>
+          ))}
+        </div>
+      )}
+    </section>
   );
 }
 
