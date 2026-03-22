bcccda61b1069cf97aa9af51ca247d714cefcb9d
RL08 <dev.rl08@outlook.com>
2026-03-22 15:26:56 +0100

    feat: Add RandomMealCard to HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,45 +1,51 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getCategories } from "@/services/mealService.js";
-import CategoryCard from "@/features/category/CategoryCard.jsx";
 import "@/pages/home/HomePage.css";
+import CategoryCard from "@/features/category/CategoryCard.jsx";
+import RandomMealCard from "@/features/meal/components/RandomMealCard.jsx";
 
 function HomePage() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const loadCategories = async () => {
-      try {
-        const categories = await getCategories();
-        setCategories(categories);
-      } catch {
-        setError("Failed to load categories...");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    loadCategories();
+  const loadCategories = useCallback(async () => {
+    try {
+      const categories = await getCategories();
+      setCategories(categories);
+    } catch {
+      setError("Failed to load categories...");
+    } finally {
+      setLoading(false);
+    }
   }, []);
+  
+  useEffect(() => {
+    loadCategories();
+  }, [loadCategories]);
 
   return (
-    <section className="categories-section">
-      <h2>Categories</h2>
-      {error && <div className="error-message">{error}</div>}
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="categories-grid">
-          {categories.map((category) => (
-            <CategoryCard
-              category={category}
-              key={category.idCategory}
-            ></CategoryCard>
-          ))}
-        </div>
-      )}
-    </section>
+    <>
+      <section className="todays-pick-section">
+        <RandomMealCard></RandomMealCard>
+      </section>
+      <section className="categories-section">
+        <h2>Categories</h2>
+        {error && <div className="error-message">{error}</div>}
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="categories-grid">
+            {categories.map((category) => (
+              <CategoryCard
+                category={category}
+                key={category.idCategory}
+              ></CategoryCard>
+            ))}
+          </div>
+        )}
+      </section>
+    </>
   );
 }
 
