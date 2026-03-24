114863080608d309f71cae1216b7bbfcff6cdefc
RL08 <dev.rl08@outlook.com>
2026-03-24 11:43:18 +0100

    feat: Add MealCard

---
+++ src/features/meal/components/MealCard.jsx
@@ -0,0 +1,26 @@
+import "@features/meal/style/MealCard.css";
+import { Link } from "react-router-dom";
+import Span from "@/components/span/Span.jsx";
+import {
+  Card,
+  CardBody,
+  CardImage,
+  CardTitle,
+} from "@/components/card/Card.jsx";
+
+function MealCard({ meal }) {
+  return (
+    <Link to={`/meal/${meal.strMeal}`}>
+      <Card variant="animated">
+        <div className="meal-card-img-wrapper">
+          <CardImage src={meal.strMealThumb} alt={meal.strMeal} />
+        </div>
+        <CardBody>
+          <CardTitle variant="wrap">{meal.strMeal}</CardTitle>
+        </CardBody>
+      </Card>
+    </Link>
+  );
+}
+
+export default MealCard;
