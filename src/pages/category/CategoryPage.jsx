ac0c08113f3399c7b504940544c4a5afe9f03f70
RL08 <dev.rl08@outlook.com>
2026-03-24 11:44:16 +0100

    feat: Add CategoryPage

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,9 +1,48 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMealsByCategory } from "@/services/mealService.js";
+import "@/pages/category/CategoryPage.css"
+import Section from "@/components/section/Section.jsx";
+import MealCard from "@/features/meal/components/MealCard.jsx";
+
 function CategoryPage() {
-    return(
-        <>
-        
-        </>
-    )
+  const { name } = useParams();
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const loadCategories = useCallback(async () => {
+    try {
+      const meals = await getMealsByCategory(name);
+      setMeals(meals);
+    } catch {
+      setError("Failed to load categories...");
+    } finally {
+      setLoading(false);
+    }
+  }, [name]);
+
+  useEffect(() => {
+    loadCategories();
+  }, [loadCategories]);
+
+  return (
+    <>
+      <Section>
+        {error && <div className="error-message">{error}</div>}
+
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="meals-grid">
+            {meals.map((meal) => (
+              <MealCard meal={meal} key={meal.idMeal}></MealCard>
+            ))}
+          </div>
+        )}
+      </Section>
+    </>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
