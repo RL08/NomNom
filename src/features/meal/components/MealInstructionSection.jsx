acc697d9654d12b87716f3e4c6fddc6e59cb351f
RL08 <dev.rl08@outlook.com>
2026-03-25 12:17:38 +0100

    fix: Improve MealInstructionSection component

--- src/features/meal/components/MealInstructionSection.jsx
+++ src/features/meal/components/MealInstructionSection.jsx
@@ -1,30 +1,24 @@
+import "@/features/meal/style/MealInstructionSection.css";
 import { H2 } from "@/components/ui/heading/Heading.jsx";
-import { Li, Ol } from "@/components/ui/list/List.jsx";
-import P from "@/components/ui/p/P.jsx";
 import Section from "@/components/ui/section/Section.jsx";
-import Span from "@/components/ui/span/Span.jsx";
-import "@features/meal/style/MealInstructionSection.css";
+import P from "@/components/ui/p/P.jsx";
 
 function MealInstructionSection({ meal }) {
-  const instructions =
-    meal.strInstructions
-      ?.split(/\r?\n/)
-      .map((s) => s.trim())
-      .filter((s) => s.length > 10) || [];
+  const instructions = meal.strInstructions?.split(/\r\n|\r|\n/) ?? [];
 
   return (
     <>
       <Section>
         <H2>Instructions</H2>
-        <Ol>
-          {instructions &&
-            instructions.map((step, i) => (
-              <Li variant="ordered dark" key={i}>
-                <Span variant="lg bold">{String(i + 1).padStart(2, "0")}</Span>
-                <Span variant="lg">{step}</Span>
-              </Li>
+        {instructions && (
+          <div className="mealinstructionsection-instruction-wrapper">
+            {instructions.map((line, index) => (
+              <P variant="dark lg mb" key={index}>
+                {line}
+              </P>
             ))}
-        </Ol>
+          </div>
+        )}
       </Section>
     </>
   );
